ae488b5eae6ce0dc8699c9aae3ee45d6e507af89
Supawan <lovelyyou68@gmail.com>
2018-01-21 16:29:20 +0700

    Fixed test

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,14 +1,8 @@
-import org.junit.Test;
-
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 public class HelloTest {
 
 
-    @Test
-    public void xxxx() {
-        assertTrue(true);
+    public void xxxx(){
     }
 
 }
